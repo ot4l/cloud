3203a00814def9301eef4eb27a3993c9c303acf4
ot4l <115358974+ot4l@users.noreply.github.com>
2022-10-15 12:54:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let s=a=>document.getElementById(a);
 
 
 function getIdFromUrl(url) { return url.match(/[-\w]{25,}/); }
-let apikey = 'AIzaSyD739-eb6NzS_KbVJq1K8ZAxnrMfkIqPyw';
+let apikey = 'AIzaSyArMlCeiAYrg9FvvxgDgBIX6Y80m3TUxm8';
 
 let get=()=>{
   getbase();
